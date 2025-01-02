b894a3292764bcb8d3af8462bc410791df0b5020
Amaryllis Graybill <samuraiham@gmail.com>
2025-01-02 05:16:07 +0000

    part 4.2

--- smartnotes-tutorial/smartnotes/urls.py
+++ smartnotes-tutorial/smartnotes/urls.py
@@ -22,5 +22,6 @@ from home import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('home.urls'))
+    path('', include('home.urls')),
+    path('smart/', include('notes.urls'))
 ]
